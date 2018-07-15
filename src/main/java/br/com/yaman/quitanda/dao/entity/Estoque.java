a59c25241905ffd47a21a7b5fe146d645cbdf1d7
nutellatux <rodrigo.lucena83@outlook.com>
2018-07-14 23:47:26 -0300

    Falta de Get e Set Manuais, Anotações nõa funcionando

--- src/main/java/br/com/yaman/quitanda/dao/entity/Estoque.java
+++ src/main/java/br/com/yaman/quitanda/dao/entity/Estoque.java
@@ -17,9 +17,9 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "ESTOQUE")
 @SequenceGenerator(name = "EstoqueSeq", sequenceName = "SEQ_COD_ESTOQUE")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
 public class Estoque {
 	
 	@Id
@@ -36,5 +36,92 @@ public class Estoque {
 	
     @Column(name = "QTD_TOTAL")
     private Integer quantidadeTotal;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public Integer getQuantidadeDisponivel() {
+		return quantidadeDisponivel;
+	}
+
+	public void setQuantidadeDisponivel(Integer quantidadeDisponivel) {
+		this.quantidadeDisponivel = quantidadeDisponivel;
+	}
+
+	public Integer getQuantidadeTotal() {
+		return quantidadeTotal;
+	}
+
+	public void setQuantidadeTotal(Integer quantidadeTotal) {
+		this.quantidadeTotal = quantidadeTotal;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((produto == null) ? 0 : produto.hashCode());
+		result = prime * result + ((quantidadeDisponivel == null) ? 0 : quantidadeDisponivel.hashCode());
+		result = prime * result + ((quantidadeTotal == null) ? 0 : quantidadeTotal.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Estoque other = (Estoque) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (produto == null) {
+			if (other.produto != null)
+				return false;
+		} else if (!produto.equals(other.produto))
+			return false;
+		if (quantidadeDisponivel == null) {
+			if (other.quantidadeDisponivel != null)
+				return false;
+		} else if (!quantidadeDisponivel.equals(other.quantidadeDisponivel))
+			return false;
+		if (quantidadeTotal == null) {
+			if (other.quantidadeTotal != null)
+				return false;
+		} else if (!quantidadeTotal.equals(other.quantidadeTotal))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Estoque [id=" + id + ", produto=" + produto + ", quantidadeDisponivel=" + quantidadeDisponivel
+				+ ", quantidadeTotal=" + quantidadeTotal + "]";
+	}
+	
+	
+    
+    
+    
+    
     
 }
--- src/main/java/br/com/yaman/quitanda/dao/entity/Produto.java
+++ src/main/java/br/com/yaman/quitanda/dao/entity/Produto.java
@@ -17,9 +17,9 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "PRODUTO")
 @SequenceGenerator(name = "ProdutoSeq", sequenceName = "SEQ_COD_PRODUTO")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
 public class Produto {
 
 	@Id
@@ -42,5 +42,116 @@ public class Produto {
     
     @Column(name = "PESO_MEDIO")
     private Double pesoMedio;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public TipoProduto getTipoProduto() {
+		return tipoProduto;
+	}
+
+	public void setTipoProduto(TipoProduto tipoProduto) {
+		this.tipoProduto = tipoProduto;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getCalorias() {
+		return calorias;
+	}
+
+	public void setCalorias(Double calorias) {
+		this.calorias = calorias;
+	}
+
+	public Double getPesoMedio() {
+		return pesoMedio;
+	}
+
+	public void setPesoMedio(Double pesoMedio) {
+		this.pesoMedio = pesoMedio;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((calorias == null) ? 0 : calorias.hashCode());
+		result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((pesoMedio == null) ? 0 : pesoMedio.hashCode());
+		result = prime * result + ((tipoProduto == null) ? 0 : tipoProduto.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (calorias == null) {
+			if (other.calorias != null)
+				return false;
+		} else if (!calorias.equals(other.calorias))
+			return false;
+		if (descricao == null) {
+			if (other.descricao != null)
+				return false;
+		} else if (!descricao.equals(other.descricao))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (pesoMedio == null) {
+			if (other.pesoMedio != null)
+				return false;
+		} else if (!pesoMedio.equals(other.pesoMedio))
+			return false;
+		if (tipoProduto == null) {
+			if (other.tipoProduto != null)
+				return false;
+		} else if (!tipoProduto.equals(other.tipoProduto))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Produto [id=" + id + ", nome=" + nome + ", tipoProduto=" + tipoProduto + ", descricao=" + descricao
+				+ ", calorias=" + calorias + ", pesoMedio=" + pesoMedio + "]";
+	}
+    
+    
 	
 }
